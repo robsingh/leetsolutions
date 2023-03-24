669da8439fd0036e9a64a38facc828e068ca9f4b
robsingh <robie10895@gmail.com>
2023-03-24 16:08:59 -0400

    Valid Anagram

---
+++ valid_anagram.py
@@ -0,0 +1,59 @@
+"""
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+typically using all the original letters exactly once.
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Input: s = "rat", t = "car"
+Output: false
+
+--->>> what if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+"""
+
+class Solution:
+    # def isAnagram(self, s:str, t:str) -> bool:
+        # s_lower = s.lower()   (O(n(logn)))
+        # t_lower = t.lower()
+
+        # if len(s_lower) == len(t_lower):
+        #     sorted_s = sorted(s_lower)
+        #     sorted_t = sorted(t_lower)
+
+        #     if sorted_s == sorted_t:
+        #         return True
+        #     else:
+        #         return False
+
+
+    def isAnagram(self, s:str, t:str) -> bool:
+        if len(s.lower()) != len(t.lower()):
+            return False
+        
+        #create a dictionary to count the occurrences of each Unicode
+        char_count = {}
+        for char in s:
+            code_point = ord(char)
+            if code_point in char_count:
+                char_count[code_point] += 1
+            else:
+                char_count[code_point] = 1
+
+        #check if each character in t appears same times as that in s
+        for char in t:
+            code_point = ord(char)
+            if code_point in char_count:
+                char_count[code_point] -= 1
+                if char_count[code_point] < 0:
+                    return False
+            else:
+                return False
+            
+
+        return True
+
+
+sol = Solution()
+print(sol.isAnagram(s="rat",t="car"))
