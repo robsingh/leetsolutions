bb97a05c86b5f6f0df5984e27e820b8ad9dd2a9b
robsingh <robie10895@gmail.com>
2023-04-07 12:09:19 -0400

    Min Depth

--- min_depth.py
+++ min_depth.py
@@ -16,7 +16,16 @@ class TreeNode:
 
 class Solution:
     def min_depth(self,root:Optional[TreeNode]) -> int:
-        pass
+        if not root:
+            return 0
+        left_depth = self.min_depth(root.left)
+        right_depth = self.min_depth(root.right)
+
+
+        if not left_depth or not right_depth:
+            return max(left_depth, right_depth) + 1
+        
+        return min(left_depth,right_depth) + 1
 
 
 root = TreeNode(3)
