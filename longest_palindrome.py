f0363ac2c3fab7ed2bc66cac9837e1027e6ce688
robsingh <robie10895@gmail.com>
2023-04-12 18:19:00 -0400

    O(n) for longest palindrome

--- longest_palindrome.py
+++ longest_palindrome.py
@@ -6,31 +6,57 @@ A string is palindromic if it reads the same forward and backward.
 
 class Solution:
     def longestPalindrome(self,s:str) -> str:
-        n = len(s)
-        max_len = 1
-        start = 0
+        # O(n^2) Solution
+        # n = len(s)
+        # max_len = 1
+        # start = 0
 
-        dp = [[False]*n for _ in range(n)]
+        # dp = [[False]*n for _ in range(n)]
         
+        # for i in range(n):
+        #     dp[i][i] = True
+
+        # for i in range(n-1):
+        #     if s[i] == s[i+1]:
+        #         dp[i][i+1] = True
+        #         max_len = 2
+        #         start = i
+
+        # for k in range(3, n+1):
+        #     for i in range(n-k+1):
+        #         j = i + k - 1
+        #         if dp[i+1][j-1] and s[i] == s[j]:
+        #             dp[i][j] = True
+        #             if k > max_len:
+        #                 max_len = k
+        #                 start = i
+
+        # return s[start:start+max_len]
+        if not s:
+            return ""
+        n = len(s)
+        start,end = 0,0
+
         for i in range(n):
-            dp[i][i] = True
+            # check for odd length palindromes
+            left,right = i,i
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+                right += 1
+            if right - left - 1 > end - start:
+                start = left + 1
+                end = right
 
-        for i in range(n-1):
-            if s[i] == s[i+1]:
-                dp[i][i+1] = True
-                max_len = 2
-                start = i
+            # check for even length palindromes
+            left,right = i,i+1
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+                right += 1
+            if right - left - 1 > end - start:
+                start = left + 1
+                end = right
 
-        for k in range(3, n+1):
-            for i in range(n-k+1):
-                j = i + k - 1
-                if dp[i+1][j-1] and s[i] == s[j]:
-                    dp[i][j] = True
-                    if k > max_len:
-                        max_len = k
-                        start = i
-
-        return s[start:start+max_len]
+        return s[start:end]
 
 
 
