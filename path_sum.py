4cc147c068e9b085933f4cdb20bf853c2273197e
robsingh <robie10895@gmail.com>
2023-04-19 16:53:51 -0700

    Adding Doc Strings and Comments

--- path_sum.py
+++ path_sum.py
@@ -15,12 +15,25 @@ class TreeNode:
 
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        """
+        Determine if there is a root to leaf path in the binary tree that sums up to targetSum.
+
+        Args:
+        - root : root node of binary tree
+        - targetSum : an int representing targetSum
+
+        Returns:
+        - a boolean value
+        """
+        # if root node is None, there is no path that sums up to targetSum
         if not root:
             return False
         
+        # if root node is a leaf node and its value equals targetSum return True
         if not root.left and not root.right and targetSum == root.val:
             return True
         
+        # recursively check the left and right subtrees for paths that sum up to targetSum
         left = self.hasPathSum(root.left, targetSum - root.val)
         right = self.hasPathSum(root.right, targetSum - root.val)
 
