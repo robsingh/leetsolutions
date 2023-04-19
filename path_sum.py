1e1e765ad2e711ed575f1823d10205a387cbdcce
robsingh <robie10895@gmail.com>
2023-04-19 15:42:38 -0700

    Read the leaf

--- path_sum.py
+++ path_sum.py
@@ -5,16 +5,39 @@ root-to-leaf path such that adding up all the values along the path equals targe
 A leaf is a node with no children.
 """
 from typing import Optional
+
 class TreeNode:
-    def __init__(self,val=0,left=None,right=None):
+    def __init__(self, val = 0, left = None, right = None):
         self.val = val
         self.left = left
         self.right = right
 
 
 class Solution:
-    def hasPathSum(self,root:Optional[TreeNode], targetSum: int) -> bool:
-        pass
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        
+        if not root.left and not root.right and targetSum == root.val:
+            return True
+        
+        left = self.hasPathSum(root.left, targetSum - root.val)
+        right = self.hasPathSum(root.right, targetSum - root.val)
+
+        return left or right
+  
 
 
-    
+root = TreeNode(5)
+root.left = TreeNode(4)
+root.right = TreeNode(8)
+root.left.left = TreeNode(11)
+root.left.left.left = TreeNode(7)
+root.left.left.right = TreeNode(2)
+root.right.left = TreeNode(13)
+root.right.right = TreeNode(4)
+root.right.right.right = TreeNode(1)
+
+sol = Solution()
+targetSum = 22
+print(sol.hasPathSum(root,targetSum))
