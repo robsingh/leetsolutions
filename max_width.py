7e7831ed524ce683e986a9fc28ec2875dcebd4a8
robsingh <robie10895@gmail.com>
2023-04-20 17:07:47 -0700

    Adding Doc Strings and Comments

--- max_width.py
+++ max_width.py
@@ -20,27 +20,37 @@ class TreeNode:
 
 class Solution:
     def widthofBinaryTree(self,root:Optional[TreeNode]) -> int:
+        """
+        Calculates max width of a binary tree.
+
+        Args: 
+            root: root of the binary tree
+
+        Returns:
+            int: max width of a binary tree 
+        """
+        
         if not root:
             return 0
         max_width = 0
+        # initialize the queue with root node and its index.
         queue = deque([(root,0)])
 
         while queue:
             level_size = len(queue)
-            left_index = queue[0][1]
+            left_index = queue[0][1] # get index of leftmost node in current level.
             for i in range(level_size):
-                node,index = queue.popleft()
+                node,index = queue.popleft() # get the next node and its index from the queue.
                 if node.left:
                     queue.append((node.left, 2 * index))
                 if node.right:
                     queue.append((node.right, 2 * index + 1))
-            
+            # index of the last processed node is the index of rightmost node.
             right_index = index
             width = right_index - left_index + 1
             max_width = max(max_width, width)
         
         return max_width
-
 
 
 root = TreeNode(1)
