2f5efc3160ae1ad94ed1018f7dc5fbec75389140
robsingh <robie10895@gmail.com>
2023-06-10 10:16:07 -0700

    Pseudo Code

--- excelsheet.py
+++ excelsheet.py
@@ -20,7 +20,18 @@ Constraints:
 
 1 <= columnNumber <= 231 - 1
 """
+"""
+Thought process:
+1. Initialize an empty string to store the column title.
+2. While the columnNumber is greater than 0:
+    -> Subtract 1 from the columnNumber to adjust it to a zero-based index.
+    -> calculate the remainder of the columnNumber divided by 26. This will give us the index of the corresponding letter in the alphabet.
+    -> Convert the index to a letter by adding it to the ASCII value of 'A'. (use chr())
+    -> add the letter to the beginning of the column string. 
+    -> divide the columnNumber by 26, discarding any remainder, to move to the next digit. 
+3. Return the column title string. 
 
+"""
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
         result = ""
