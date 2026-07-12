5cc15d2e71b0d78bd28b8120da0d04c8cb7cdece
robsingh <robie10895@gmail.com>
2026-07-12 13:54:28 -0400

    O(n.k) solution

--- group_anagrams.py
+++ group_anagrams.py
@@ -6,22 +6,33 @@ Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 """
 
 from typing import List
+from collections import defaultdict
 
-class Solution:
-    def groupAnagrams(self, strs:List[str]) -> List[List[str]]:
-        groups = {}
-        for s in strs:
-            key = ''.join(sorted(s))
-            if key in groups:
-                groups[key].append(s)
-            else:
-                groups[key] = [s]
+# O(n.k log k) - sorting method
+# class Solution:
+#     def groupAnagrams(self, strs:List[str]) -> List[List[str]]:
+#         groups = {}
+#         for s in strs:
+#             key = ''.join(sorted(s))
+#             if key in groups:
+#                 groups[key].append(s)
+#             else:
+#                 groups[key] = [s]
 
-        return list(groups.values())
+#         return list(groups.values())
 
+# sol = Solution()
+# print(sol.groupAnagrams(strs=["eat","tea","tan","ate","nat","bat"]))
 
+#linear scan - O(n.k)
+def groupAnagrams(strs):
+    groups = defaultdict(list)
+    for s in strs:
+        count = [0] * 26
+        for character in s:
+            count[ord(character) - ord('a')] += 1
+        key = tuple(count)
+        groups[key].append(s)
+    return list(groups.values())
 
-
-
-sol = Solution()
-print(sol.groupAnagrams(strs=["eat","tea","tan","ate","nat","bat"]))
+print(groupAnagrams(strs=["eat","tea","tan","ate","nat","bat"]))    
